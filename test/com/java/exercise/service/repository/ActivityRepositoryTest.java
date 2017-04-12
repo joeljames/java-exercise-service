7c0877b803bea7585619c964349d012812d03aa6
joeljames <joeljames1985@gmail.com>
2017-04-12 07:10:58 -0700

    Add example unit test and add ant command to run junit tests

---
+++ test/com/java/exercise/service/repository/ActivityRepositoryTest.java
@@ -0,0 +1,18 @@
+package com.java.exercise.service.repository;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class ActivityRepositoryTest {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @Test
+    public void test() {
+        assertEquals("this", "this");
+    }
+}
